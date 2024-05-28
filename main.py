da723c575a9d26b6807ef4bcb39109f05b5f3c41
Omar Essam <126985629+Omar-Essam4@users.noreply.github.com>
2024-05-28 13:40:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from keras.models import load_model
 model = load_model('CNN_scratch.h5')
 model2 = load_model('model.h5')
 model3 = load_model('CNN_Dense_NN.h5')
+model4 = load_model('vgg-16.h5')
+model5 = load_model('inception_v3.h5')
 
 
 def compute_metrics(predict):
@@ -26,7 +28,10 @@ def preprocess_image(img, target_size):
 
 # Define a function to make predictions
 def predict(image, model):
-    processed_image = preprocess_image(image, target_size=(64, 64))  # Ensure the target size matches your model input
+    if model != model5:
+        processed_image = preprocess_image(image, target_size=(64, 64))  # Ensure the target size matches your model input
+    else:
+        processed_image = preprocess_image(image, target_size=(75, 75))  # Ensure the target size matches your model input
     prediction = model.predict(processed_image)
     return prediction
 
@@ -47,9 +52,13 @@ if uploaded_file is not None:
     prediction = predict(image, model)
     prediction2 = predict(image, model2)
     prediction3 = predict(image, model3)
+    prediction4 = predict(image, model4)
+    prediction5 = predict(image, model5)
     print(prediction)
     print(prediction2)
     print(prediction3)
+    print(prediction4)
+    print(prediction5)
 
     # Assuming the model returns a class index, you might need to map it to class names
     class_name = ""
@@ -69,3 +78,14 @@ if uploaded_file is not None:
     else:
         class_name = "Uninfected"
     st.write(f"CNN Dense Prediction: {compute_metrics(prediction3)}, {class_name}")
+    if prediction4[0] > 0.5:
+        class_name = "Parasitized"
+    else:
+        class_name = "Uninfected"
+    st.write(f"VGG-16 Prediction: {compute_metrics(prediction4)}, {class_name}")
+    if prediction5[0] > 0.5:
+        class_name = "Parasitized"
+    else:
+        class_name = "Uninfected"
+    st.write(f"Inception V3: {compute_metrics(prediction5)}, {class_name}")
+
